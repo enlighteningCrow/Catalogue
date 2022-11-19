a10a0418e4d3c56e08c16ae4e1dc512debf13441
twistingcamel <twistingcamel@gmail.com>
2022-11-19 20:58:37 +0700

    Removed the debugging println lines

--- src/main/java/org/AOOPProject/NavigationButton.java
+++ src/main/java/org/AOOPProject/NavigationButton.java
@@ -28,26 +28,28 @@ public abstract class NavigationButton extends JButton
 		this.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				for (int i = 0; i < 100; ++i)
-					System.out.print('<');
-				System.out.print('\n');
-				for (int i = 0; i < NavigationButton.this.group.dsf.hist.size(); ++i) {
-					System.out.println((NavigationButton.this.group.dsf.hist.currentIndex == i ? "-> " : "   ")
-							+ NavigationButton.this.group.dsf.hist.get(i));
-				}
-				System.out.print('\n');
+				// for (int i = 0; i < 100; ++i)
+				// System.out.print('<');
+				// System.out.print('\n');
+				// for (int i = 0; i < NavigationButton.this.group.dsf.hist.size(); ++i) {
+				// System.out.println((NavigationButton.this.group.dsf.hist.currentIndex == i ?
+				// "-> " : " ")
+				// + NavigationButton.this.group.dsf.hist.get(i));
+				// }
+				// System.out.print('\n');
 				manipulateHistory();
 				if (group.dsf != null)
 					group.dsf.getBridge().update();
 				// Note: The following will be called anyways
 				// NavigationButton.updateButtons();
-				for (int i = 0; i < NavigationButton.this.group.dsf.hist.size(); ++i) {
-					System.out.println((NavigationButton.this.group.dsf.hist.currentIndex == i ? "-> " : "   ")
-							+ NavigationButton.this.group.dsf.hist.get(i));
-				}
-				for (int i = 0; i < 100; ++i)
-					System.out.print('>');
-				System.out.print('\n');
+				// for (int i = 0; i < NavigationButton.this.group.dsf.hist.size(); ++i) {
+				// System.out.println((NavigationButton.this.group.dsf.hist.currentIndex == i ?
+				// "-> " : " ")
+				// + NavigationButton.this.group.dsf.hist.get(i));
+				// }
+				// for (int i = 0; i < 100; ++i)
+				// System.out.print('>');
+				// System.out.print('\n');
 			}
 		});
 		if (!group.allButtons.contains(this))
